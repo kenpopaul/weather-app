65d4349c6e0960fe812cbd624c69e7d67db46f00
Your NamePaul <kenpopaul@yahoo.co.uk>
2022-07-16 07:42:47 +0100

    Temp location and actual date added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ const api = {
 }
 
 function App() {
+
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`
+}
+
   return (
     <div className="app">
       <main>
@@ -14,6 +27,10 @@ function App() {
             className='search-bar'
             placeholder='Search...'
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Newquay, UK</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
