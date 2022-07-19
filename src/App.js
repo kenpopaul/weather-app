eb032dfaf5b610371c408101d088f735673bd84c
Your NamePaul <kenpopaul@yahoo.co.uk>
2022-07-19 09:21:24 +0100

    date corrected

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
 const dateBuilder = (d) => {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   let day = days[d.getDay()];
   let date = d.getDate();
