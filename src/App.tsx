b32d0d79256b864558fb2abd2632c70a4bf4277c
Paul Nix <kenpopaul@yahoo.co.uk>
2023-06-19 07:56:03 +0100

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,102 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const api = {
   key: "15706b4115981325eed7a8e8d00c9aec",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
+        .then((res) => res.json())
+        .then((result) => {
           setWeather(result);
-          setQuery('');
+          setQuery("");
           console.log(result);
         });
     }
-  }
+  };
 
-const dateBuilder = (d) => {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-  return `${day} ${date} ${month} ${year}`
-}
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   return (
-    <div className = {(typeof (weather as any).main != "undefined") ? (((weather as any).main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div
+      className={
+        typeof (weather as any).main != "undefined"
+          ? (weather as any).main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
       <main>
         <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
-        {(typeof (weather as any).main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{(weather as any).name}, {(weather as any).sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round((weather as any).main.temp)}°c
-            </div>
-              <div className="weather">{(weather as any).weather[0].main} 
-                <div className="wind">Wind speed: {Math.round((weather as any).wind.speed)} mph</div>
+        {typeof (weather as any).main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {(weather as any).name}, {(weather as any).sys.country}
               </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round((weather as any).main.temp)}°c
+              </div>
+              <div className="weather">
+                {(weather as any).weather[0].main}
+                <div className="wind">
+                  Wind speed: {Math.round((weather as any).wind.speed)} mph
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        ) : ('')}
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
