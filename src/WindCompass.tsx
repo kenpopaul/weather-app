dee5d201d20b10054f71a0d0ccbd1e1ad980d04a
Paul Nix <kenpopaul@yahoo.co.uk>
2023-07-20 21:40:55 +0100

    correct compass direction

--- src/WindCompass.tsx
+++ src/WindCompass.tsx
@@ -6,9 +6,9 @@ interface WindCompassProps {
 }
 
 const WindCompass: React.FC<WindCompassProps> = ({ windDirection }) => {
-  const compassStyles: React.CSSProperties = {
-    transform: `rotate(${windDirection}deg)`,
-  };
+  // Calculate the end coordinates of the line based on wind direction
+  const x2 = 50 + Math.sin((windDirection * Math.PI) / 180) * 30;
+  const y2 = 50 - Math.cos((windDirection * Math.PI) / 180) * 30;
 
   return (
     <div className="wind-compass">
@@ -16,17 +16,23 @@ const WindCompass: React.FC<WindCompassProps> = ({ windDirection }) => {
         className="compass-icon"
         viewBox="0 0 100 100"
         xmlns="http://www.w3.org/2000/svg"
-        style={compassStyles}
       >
-        {/* Add the SVG paths for the compass icon */}
-        {/* North */}
+        {/* Compass shape */}
+        <circle
+          cx="50"
+          cy="50"
+          r="45"
+          fill="none"
+          stroke="black"
+          strokeWidth="3"
+        />
         <path d="M50,5 L55,15 L45,15 L50,5" fill="black" />
-        {/* East */}
         <path d="M95,50 L85,55 L85,45 L95,50" fill="black" />
-        {/* South */}
         <path d="M50,95 L55,85 L45,85 L50,95" fill="black" />
-        {/* West */}
         <path d="M5,50 L15,55 L15,45 L5,50" fill="black" />
+
+        {/* Hand of the compass */}
+        <line x1="50" y1="50" x2={x2} y2={y2} stroke="black" strokeWidth="2" />
       </svg>
     </div>
   );
