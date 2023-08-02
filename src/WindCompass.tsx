3932e610914dcae9bb0730589b018b0f25c8f487
Paul Nix <kenpopaul@yahoo.co.uk>
2023-08-02 09:59:10 +0100

    style:  compass nicer & handle length correct

--- src/WindCompass.tsx
+++ src/WindCompass.tsx
@@ -5,28 +5,47 @@ interface WindCompassProps {
 }
 
 const WindCompass: React.FC<WindCompassProps> = ({ windDirection }) => {
-  const handleLength = 40;
+  const compassRadius = 60; // Adjust the compass radius here
+  const handleLength = 55; // Adjust the handle length here
   const handleX =
     Math.cos((windDirection - 90) * (Math.PI / 180)) * handleLength;
   const handleY =
     Math.sin((windDirection - 90) * (Math.PI / 180)) * handleLength;
 
-  const textOffset = 30;
+  const textOffset = 45;
+  const cardinalDirectionFontSize = 14; // Adjust the font size here
+  const outerCircleStrokeWidth = 8; // Adjust the stroke width of the outer circle here
 
   return (
     <svg
       className="wind-compass"
-      width="100" // Width of compass
-      height="100" // Height of compass
-      viewBox="-50 -50 100 100" // Viewbox for a larger compass
+      width={2 * compassRadius + 2 * outerCircleStrokeWidth}
+      height={2 * compassRadius + 2 * outerCircleStrokeWidth}
+      viewBox={`${-compassRadius - outerCircleStrokeWidth} ${
+        -compassRadius - outerCircleStrokeWidth
+      } ${2 * compassRadius + 2 * outerCircleStrokeWidth} ${
+        2 * compassRadius + 2 * outerCircleStrokeWidth
+      }`}
       xmlns="http://www.w3.org/2000/svg"
     >
-      {/* Outer circle */}
-      <circle cx="0" cy="0" r="44" fill="grey" strokeWidth="6" stroke="grey" />
-
       {/* Compass circle */}
-      <circle cx="0" cy="0" r="40" fill="black" stroke="#fff" strokeWidth="2" />
-
+      <circle
+        cx="0"
+        cy="0"
+        r={compassRadius}
+        fill="black"
+        stroke="#fff"
+        strokeWidth="2"
+      />
+      {/* Outer circle */}
+      <circle
+        cx="0"
+        cy="0"
+        r={compassRadius + outerCircleStrokeWidth / 2} // Adjust the outer circle radius
+        fill="transparent"
+        strokeWidth={outerCircleStrokeWidth}
+        stroke="grey"
+      />
       {/* Handle with small red circle at the end */}
       <line
         x1="0"
@@ -36,19 +55,43 @@ const WindCompass: React.FC<WindCompassProps> = ({ windDirection }) => {
         stroke="#fff"
         strokeWidth="2"
       />
-      <circle cx={handleX} cy={handleY} r="4" fill="red" />
-
+      <circle cx={handleX} cy={handleY} r="3" fill="red" />{" "}
+      {/* Adjust the size of the red circle */}
       {/* Add cardinal directions */}
-      <text x="0" y={-textOffset} textAnchor="middle" fill="#fff" fontSize="10">
+      <text
+        x="0"
+        y={-textOffset}
+        textAnchor="middle"
+        fill="grey" // Adjust the color here
+        fontSize={cardinalDirectionFontSize} // Use the updated font size here
+      >
         N
       </text>
-      <text x="0" y={textOffset} textAnchor="middle" fill="#fff" fontSize="10">
+      <text
+        x="0"
+        y={textOffset}
+        textAnchor="middle"
+        fill="grey" // Adjust the color here
+        fontSize={cardinalDirectionFontSize} // Use the updated font size here
+      >
         S
       </text>
-      <text x={textOffset} y="4" textAnchor="middle" fill="#fff" fontSize="10">
+      <text
+        x={textOffset}
+        y="5"
+        textAnchor="middle"
+        fill="grey" // Adjust the color here
+        fontSize={cardinalDirectionFontSize} // Use the updated font size here
+      >
         E
       </text>
-      <text x={-textOffset} y="4" textAnchor="middle" fill="#fff" fontSize="10">
+      <text
+        x={-textOffset}
+        y="5"
+        textAnchor="middle"
+        fill="grey" // Adjust the color here
+        fontSize={cardinalDirectionFontSize} // Use the updated font size here
+      >
         W
       </text>
     </svg>
